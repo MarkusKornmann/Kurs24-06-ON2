012ed82fa38af7f8e7f09dca45bec9c9a0eba119
MASKO <markus-kornmann@outlook.com>
2024-10-21 17:29:41 +0200

    Zusatzaufgabe hinzugefügt

--- Statische Website mit HTML und CSS/JavaScript/react/Hausaufgaben-21-10-24/hausaufgaben-react/src/App.js
+++ Statische Website mit HTML und CSS/JavaScript/react/Hausaufgaben-21-10-24/hausaufgaben-react/src/App.js
@@ -4,16 +4,21 @@ import './App.css';
 
 function App() {
   let hihi = "Hab einen schönen Tag!";
-  
+  const handleClick = () => {
+    alert("Button wurde geklickt!");
+  };
   return (
     <div>
         <img src={logo} className="App-logo" alt="logo" />
-         {}
+
         <h1>Willkommen zu meiner ersten React-App</h1>
-         {}
+      
         <h2>{hihi}</h2>
-        {}
+    
         <h3>Das Ergebnis von 7 + 13 ist: {7 + 13}</h3>
+     
+      <button onClick={handleClick}>Klick mich</button>
+        
      </div>
    );
 }
